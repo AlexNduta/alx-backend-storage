a288c78e750609bb3c02fda6af185b5b297a4331
was <was@localhost.localdomain>
2024-03-22 15:59:04 +0300

    fixing

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- create a table `users` if it does not exit
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREAMENT PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE, 
     name VARCHAR(255)
 );
