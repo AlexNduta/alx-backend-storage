7f132937499979819d9d068fa5d52e783566e460
was <was@localhost.localdomain>
2024-03-23 13:17:10 +0300

    working

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- select data from set using origin and order by fans
 
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
