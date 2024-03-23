cae6c96c2bd3a4e7026dd056e4d8354371b66f9a
was <was@localhost.localdomain>
2024-03-23 15:49:35 +0300

    working

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- list all bands with Glam Rock as their main style ranked by their longevity
-SELECT band_name, YEAR(split)  YEAR(formed) AS lifespan
+SELECT band_name, YEAR(split) - YEAR(formed) AS lifespan
 FROM metal_bands
 WHERE style = "%Glam rock%"
 ORDER BY lifespan DESC;
