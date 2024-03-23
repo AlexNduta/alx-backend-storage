715863e24f6f0c930a8981ef285aeacb413bfdf9
was <was@localhost.localdomain>
2024-03-23 15:36:11 +0300

    working

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- list all bands with Glam Rock as their main style ranked by their longevity
 SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE "%Glam rock%"
+WHERE style = "Glam rock"
 ORDER BY lifespan DESC;
