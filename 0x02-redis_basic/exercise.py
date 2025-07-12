26ad37e32776848942872893f892f1bc1585fe4c
was <was@localhost.localdomain>
2025-07-12 16:56:46 +0300

    getting and specifying the type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 
 import redis
 import uuid
-from typing import Union 
+from typing import Union, Optional 
 """
     -setup redis for our code
 """
@@ -17,3 +17,31 @@ class Cache():
         self._redis.set(new_uuid, data)
 
         return new_uuid
+
+    def get(self, key: str, fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
+        value= self._redis.get(key)
+
+        if fn:
+            value = fn(value)
+
+    def get_str(self, key: str) -> str:
+        return self.get(key, fn=str)
+
+    def get_int(self, key: str) -> int:
+        return sef.get(key, fn=int)
+
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
+
+
